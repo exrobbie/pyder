464bb66728d65095401613559076e309b23da6c4
litianao <litianao@haizhi.com>
2016-09-06 19:47:09 +0800

    added ipython

--- pac.py
+++ pac.py
@@ -1,15 +1,3 @@
-class Fibo(object):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def series(self):
-        while(True):
-            yield self.b
-            self.a, self.b = self.b, self.a+self.b
-
-f = Fibo(0, 1)
-
-for x in f.series():
-    if x < 100:
-        print(x)
+name = input('please enter your name')
+print(name)
+input('press any key to exit')
